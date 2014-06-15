026f6b79fc77e45d9cb18ac79904313a0f5ed277
Seth Yoder <seth.a.yoder@gmail.com>
2014-06-15 09:22:45 -0400

    Made the arguments parser more sane. I've been craving Haskell... we should figure out where to start on this project.

--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ exit :: IO a
 exit = SysExit.exitWith (SysExit.ExitSuccess)
 
 helpStr :: IO ()
-helpStr = putStrLn "usage: rogue_nads [-h] [-v]"
+helpStr = putStrLn "usage: rogue_nads [-h] [--version]"
 
 versionStr :: IO ()
 versionStr = putStrLn "rogue_nads v.0.0.1"
@@ -15,11 +15,20 @@ versionStr = putStrLn "rogue_nads v.0.0.1"
 runProgram :: IO ()
 runProgram = putStrLn "Ideally this program will do something eventually."
 
-parse :: [String] -> IO a
-parse [] = runProgram >> exit
-parse (x:xs)
-    | x == "-h" = helpStr >> parse xs
-    | x == "-v" = versionStr >> parse xs
-    | otherwise = SysExit.exitWith (SysExit.ExitFailure 1)
+--Convert lists of arguments like this: ["-abc","-d","-ef"] into this:
+----["-a","-b","-c","-d","-e","-f"]
+splitArgs :: [String] -> [String]
+splitArgs argList = (argList >>= (reverse . splitArgs'))
+    where splitFunc ls arg = ('-':[arg]):ls --given [] and "abc", returns ["-a"].
+          splitArgs' (x:xs) = if x == '-' && (head xs /= '-') && length xs > 1 --Make sure it's not a double-dash arg
+                              then foldl splitFunc [] xs
+                              else [(x:xs)]
 
-main = SysEnv.getArgs >>= parse
+parse :: [String] -> IO a
+parse [] = runProgram >> exit --with no arguments left, run the program.
+parse (x:xs)
+    | x == "-h" || x == "--help" = helpStr >> exit --Typically programs exit after -h or --help.
+    | x == "--version" = versionStr >> exit
+    | otherwise = helpStr >> SysExit.exitWith (SysExit.ExitFailure 1) -- given an unrecognized argument
+
+main = SysEnv.getArgs >>= (return . splitArgs) >>= parse
